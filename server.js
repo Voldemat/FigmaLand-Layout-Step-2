fcbab0c97fd9d6260c52e32a1219e2bbc715726a
unknown <viocan2005@gmail.com>
2021-09-02 21:49:00 +0300

    'init_config'

---
+++ server.js
@@ -0,0 +1,19 @@
+const http  = require("http");
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("src"))
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html")
+})
+
+
+const httpServer = http.createServer(app)
+
+httpServer.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})
